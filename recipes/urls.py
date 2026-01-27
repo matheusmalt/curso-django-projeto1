9f992bb537952575bdb8d51819b13d7f8e19b47a
Matheus Malta <matheusmalta.pessoal@gmail.com>
2026-01-26 23:13:24 -0300

    Rotas HTTP: 'home, about e contatcs' criadas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from recipes.views import home, contatcs, about
 
 urlpatterns = [
-    path()    
+    path('', home),
+    path('contatcs/', contatcs),
+    path('about/', about),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    pass
+    return HttpResponse("<h1>Página Home</h1>")
 
 def contatcs(request):
-    pass
+    return HttpResponse("<h1>Página de Contatos</h1>")
 
 def about(request):
-    pass
+    return HttpResponse("<h1>Página de Sobre</h1>")
 
