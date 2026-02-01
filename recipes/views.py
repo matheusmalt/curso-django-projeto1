539ee311e24a46b9655e8e8da4cce8a428fc8698
Matheus Malta <matheusmalta.pessoal@gmail.com>
2026-01-31 23:37:50 -0300

    CSS concluído. Criação do arquivo global/base.html. Arquivo global/home.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
