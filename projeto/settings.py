3c6fea1edb6af07d46f22c98921eb3c7dfea4239
Matheus Malta <matheusmalta.pessoal@gmail.com>
2026-02-01 18:49:15 -0300

    Bloco de código base 'Conexão com banco de dados PostgreSQL'

--- projeto/settings.py
+++ projeto/settings.py
@@ -82,6 +82,16 @@ DATABASES = {
     }
 }
 
+'''DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
